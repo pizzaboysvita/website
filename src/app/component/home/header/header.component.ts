9dbb884ca966fe73f07508a90e637e0ef3da5d3d
stoswebsolutions <stoswebsolutions@gmail.com>
2025-08-12 12:37:55 +0530

    header menu changes completed

--- src/app/component/home/header/header.component.ts
+++ src/app/component/home/header/header.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
