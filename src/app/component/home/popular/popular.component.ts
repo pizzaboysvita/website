6a4c8236fb9971015c701967f735537cf8c7be17
stoswebsolutions <stoswebsolutions@gmail.com>
2025-08-12 12:38:40 +0530

    popular integrated

---
+++ src/app/component/home/popular/popular.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+import { HomeService } from "../../../services/home.service";
+
+@Component({
+  selector: "app-popular",
+  imports: [],
+  templateUrl: "./popular.component.html",
+  styleUrl: "./popular.component.scss",
+})
+export class PopularComponent {
+  public categories: any[] = [];
+
+  @Output() categorySelected = new EventEmitter<number>();
+
+  constructor(private apiService: HomeService) {}
+
+  ngOnInit(): void {
+    this.apiService.getCategories().subscribe((response) => {
+      if (response && response.categories) {
+        this.categories = response.categories.map((cat: any) => ({
+          ...cat,
+          imageLoaded: false,
+        }));
+      }
+    });
+  }
+
+  selectCategory(categoryId: any) {
+    this.categorySelected.emit(categoryId);
+    console.log(`Category selected: ${categoryId}`);
+  }
+}
