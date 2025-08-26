487ee6f9ef3870af8cbb7c242e139e7e5c799c5b
swamyvitasoft <swamyvitasoft@gmail.com>
2025-08-26 14:43:13 +0530

    header ordernow button

--- src/app/component/home/header/header.component.ts
+++ src/app/component/home/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Router, RouterModule } from "@angular/router";
 import { CartService } from "../../../services/cart.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { OrderComponent } from "../../../order/order.component";
 
 @Component({
   selector: "app-header",
@@ -13,7 +15,11 @@ import { CartService } from "../../../services/cart.service";
 export class HeaderComponent {
   cartCount = 0;
 
-  constructor(private cartService: CartService, private router: Router) {}
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.cartService.cartItems$.subscribe((items) => {
@@ -28,5 +34,25 @@ export class HeaderComponent {
   }
   goToWishList() {
     this.router.navigate(["/wishlist"]);
+  }
+
+  openOrderDialog() {
+    this.modalService.open(OrderComponent, { size: "lg", centered: true });
+  }
+  initMap() {
+    const map = new google.maps.Map(
+      document.getElementById("map") as HTMLElement,
+      {
+        center: { lat: 17.385044, lng: 78.486671 }, // Example coordinates (Hyderabad)
+        zoom: 12,
+      }
+    );
+
+    // Example marker
+    new google.maps.Marker({
+      position: { lat: 17.385044, lng: 78.486671 },
+      map,
+      title: "Pizza Boys",
+    });
   }
 }
